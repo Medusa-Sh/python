0ad2dfdb53aeab82a14f41de65033cbf28b42da6
Medusa-Sh <kanankananana6@gmail.com>
2026-04-24 23:16:17 +0900

    Add files via upload

---
+++ pas.py
@@ -0,0 +1,14 @@
+for x in range(10):#literate for loop
+    if x%20==0:#condition1
+        print("twist")
+
+    elif x%15==0:#condition2
+        pass#pass statement
+
+    elif x%5==0:#condition3
+        print("fizz")#display result
+
+    elif x%3==0:#condition4
+        print("buzz")#display result
+    else:#condition5
+        print(x)
