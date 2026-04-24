a2442b19e4215c676107da4041f426b4d005991f
Medusa-Sh <kanankananana6@gmail.com>
2026-04-24 23:07:23 +0900

    Add files via upload

---
+++ brake.py
@@ -0,0 +1,10 @@
+#take user imput
+a=input("Enter a word:")
+#program to checkbreak keyword
+for i in a:#iterate for loop
+    if (i=="A"):#condition1
+        print("A found")
+    break #break statement
+else:
+    #display result
+    print("A not found")
