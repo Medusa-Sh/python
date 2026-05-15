1972aaacd85b994a9d54a159a2fccb6aaa3cfa6d
Medusa-Sh <kanankananana6@gmail.com>
2026-05-16 00:30:03 +0900

    Add files via upload

---
+++ mathh.py
@@ -0,0 +1,7 @@
+import math
+sin=30
+cos=input("Enter the value of cos:")
+tan=input("Enter the value of tan:")
+print("The value of sin is:",math.sin(sin))
+print("The value of cos is:",math.cos(float(cos)))
+print("The value of tan is:",math.tan(float(tan)))
