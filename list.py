d9587307a5b3d6820c6192eb3a6f98baa2bedcb2
Medusa-Sh <kanankananana6@gmail.com>
2026-05-22 23:17:04 +0900

    Add files via upload

---
+++ list.py
@@ -0,0 +1,16 @@
+#Create a empty list
+empty_list = []
+print()
+
+#a list of numbers
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+#use*operators
+triples=[1, 2, 3] * 3
+print(triples)
+
+#reserve the given list
+aList=[1, 2, 3, 4, 5]
+aList=aList[::-1]
+print(aList,"\n")
