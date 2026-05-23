0cf1b87c5c4913269f1d81d6dfefa36eb47bdec0
Medusa-Sh <kanankananana6@gmail.com>
2026-05-23 18:24:55 +0900

    Add files via upload

---
+++ myset.py
@@ -0,0 +1,17 @@
+my_set={1,2,3}
+print(my_set)
+
+my_set={1,0,"Hello",(1,2,3,)}
+print(my_set)
+
+my_set={1,2,3,4,3,2}
+print(my_set)
+
+my_set=set([1,2,3,2])
+print(my_set,"\n")
+
+num_set=set([0,1,3,4,5])
+print("Original set:",num_set)
+num_set.pop()
+print("After removing an element:")
+print(num_set,"\n")
