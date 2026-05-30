e352d09d5d528454d160d98321eb2afc789eccf8
Medusa-Sh <kanankananana6@gmail.com>
2026-05-30 18:44:48 +0900

    Add files via upload

---
+++ se.py
@@ -0,0 +1,8 @@
+setx={"1","2","3"}
+sety={"3","4"}
+print("Original elements are:")
+print(setx)
+print(sety)
+print("\n Intersection of setx and sety is:")
+setz=setx.intersection(sety)
+print(setz)
