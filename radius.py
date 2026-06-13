5b881913d3873e318d0b9f0fc35c68501d05b6a2
Medusa-Sh <kanankananana6@gmail.com>
2026-06-13 18:43:29 +0900

    Add files via upload

---
+++ radius.py
@@ -0,0 +1,7 @@
+class radius:
+    def __init__(self, radius):
+        self.radius = radius
+
+Diameter=int(input("Enter the diameter of the circle:"))
+radius=Diameter/2
+print("The radius of the circle is:",radius) 
