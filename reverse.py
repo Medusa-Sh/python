d293b0994201f7169b3819eb9a2439699d8ca901
Medusa-Sh <kanankananana6@gmail.com>
2026-06-20 19:36:20 +0900

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,5 @@
+class reverse:
+    original=input("Enter a string to reverse")
+    print("The original letter is:",original)
+    reversed_text= ''.join(reversed(original))
+    print("Reversed text is",reversed_text)
