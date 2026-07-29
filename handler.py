fcefaea035a0d03c854d182a97dad774cba95acc
Medusa-Sh <kanankananana6@gmail.com>
2026-07-30 02:08:54 +0900

    Add files via upload

---
+++ handler.py
@@ -0,0 +1,24 @@
+from tkinter import*
+
+
+
+window=Tk()
+window.title("Event handler")
+window.geometry("100x100")
+
+def handle_keypress(event):
+    """ print the character to the key pressed"""
+    print(event.char)
+
+
+
+window.bind("<Key>", handle_keypress )
+
+def handle_click (event):
+    print("\n the botton is clicked")
+
+button=Button(text="Click me")
+button.pack()
+button.bind("<Button-1>",handle_click)
+
+window.mainloop()
